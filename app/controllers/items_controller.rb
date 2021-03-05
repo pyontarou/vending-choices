cc15cf80082e0766fc2457c3a28cf28721c3eeae
pyontarou <kooooo7150@gmail.com>
2021-03-05 22:39:45 +0900

    itemsコントローラーの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,24 @@
 class ItemsController < ApplicationController
+  # before_action :authenticate_user!
+  before_action :search_alcohol, only: [:index, :search]
+  before_action :search_food, only: [:index, :search]
 
   def index
-    
+    @alcohols = Alcohol.all
+    @foods = Food.all
   end
 
   def search
+    @results = @a.result.includes(:item)
+    @results = @f.result.includes(:item)
+  end
+
+  private
+
+  def search_alcohol
+    @a = Alcohol.ransack(params[:q])
+  end
+  def  search_food 
+    @f = Food.ransack(params[:q])
   end
 end
