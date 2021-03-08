b011ce2be21edfe7a839f52934743a8b8cf4778d
pyontarou <kooooo7150@gmail.com>
2021-03-08 15:45:23 +0900

    ActiveStorageの導入

--- Gemfile
+++ Gemfile
@@ -63,3 +63,6 @@ end
 gem 'devise'
 gem 'active_hash'
 gem 'ransack'
+gem 'pry-rails'
+gem 'mini_magick'
+gem 'image_processing','~> 1.2'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_020831) do
+ActiveRecord::Schema.define(version: 2021_03_08_004048) do
+
+  create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "alcohols", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "category", null: false
@@ -21,7 +42,7 @@ ActiveRecord::Schema.define(version: 2021_03_05_020831) do
     t.string "company_name", null: false
     t.string "alcohol_type", null: false
     t.string "taste_type", null: false
-    t.string "image", null: false
+    t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -33,7 +54,7 @@ ActiveRecord::Schema.define(version: 2021_03_05_020831) do
     t.string "where_from", null: false
     t.string "company_name", null: false
     t.string "food_type", null: false
-    t.string "image", null: false
+    t.string "image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -71,6 +92,7 @@ ActiveRecord::Schema.define(version: 2021_03_05_020831) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "alcohols"
   add_foreign_key "items", "foods"
 end
