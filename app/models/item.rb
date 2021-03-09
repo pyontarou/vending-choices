81f87ac2aa352863612e00c7ca10f92d9a81bc56
pyontarou <kooooo7150@gmail.com>
2021-03-09 16:31:58 +0900

    アソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :alcohol
   belongs_to :food
-
-
+  belongs_to :order
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+  has_many :item
+  belongs_to :user
 end
