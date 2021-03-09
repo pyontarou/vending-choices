86e83c6f7af13834045e9ebaf7158c4fd2a88787
pyontarou <kooooo7150@gmail.com>
2021-03-09 16:28:30 +0900

    orderテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_004048) do
+ActiveRecord::Schema.define(version: 2021_03_09_072011) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -68,6 +68,15 @@ ActiveRecord::Schema.define(version: 2021_03_08_004048) do
     t.index ["food_id"], name: "index_items_on_food_id"
   end
 
+  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_orders_on_item_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "prefecture_code", null: false
@@ -95,4 +104,6 @@ ActiveRecord::Schema.define(version: 2021_03_08_004048) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "alcohols"
   add_foreign_key "items", "foods"
+  add_foreign_key "orders", "items"
+  add_foreign_key "orders", "users"
 end
