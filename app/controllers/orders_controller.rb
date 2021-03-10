9020e95db85cb353914a3dfcb3a2499cc5308a88
pyontarou <kooooo7150@gmail.com>
2021-03-10 23:02:58 +0900

    クレジットカード決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,26 +7,37 @@ class OrdersController < ApplicationController
   end
   
   def create
-    @item = Item.new
     @order = Order.new(order_params)
-    if  @order.save
-      redirect_to root_path
+    if  @order.valid?
+      pay_item
+      @order.save
+      return redirect_to root_path
     else
-      render :index
+      render 'index'
     end
   end
-
+  
   def destroy
     @item = Order.find(params[:id]).item
     current_user.unorder(@item)
   end
   
+  
   private
   def set_item
     @item = Item.find(params[:item_id])  
   end
-
+  
   def order_params
-    params.require(:order).permit(:image).merge(user_id: current_user.id,item_id: params[:item_id])
+    params.permit(:image).merge(token: params[:token], user_id: current_user.id,item_id: params[:item_id])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_params[:token],
+      currency: 'jpy' ) 
+  end
+  
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
+  attr_accessor :token
   has_many :items
   belongs_to :user
+  validates :token, presence: true
+  
 end
