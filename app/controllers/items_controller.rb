d8c95c4857027bacd8ddf2d09eec50ffe88f7e5e
pyontarou <kooooo7150@gmail.com>
2021-03-11 23:02:44 +0900

    コントローラーへの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,20 @@
 class ItemsController < ApplicationController
   # before_action :authenticate_user!
   before_action :search_item, only: [:index, :search]
+  before_action :set_item, only: [:index, :search]
+  before_action :set_birthday, only: [:index, :search]
  
+  require 'happybirthday'
 
   def index
-    @items = Item.all
     set_item_column
-    
+   
   end
 
   def search 
       @results = @i.result.limit(9)
-      @items = Item.all
       set_item_column
       @order = Order.new
-      @items.each
   end
 
   private
@@ -25,5 +25,15 @@ class ItemsController < ApplicationController
 
   def set_item_column
     @item_category = Item.select("category").distinct
+  end
+
+  def set_item
+    @items = Item.all
+  end
+
+  def set_birthday
+    @user = User.new
+    birthday = Happybirthday.born_on(current_user.birth_day.to_s)
+    @birthday = birthday.age.years_old 
   end
 end
